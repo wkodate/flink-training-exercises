0fabbb67be609c4a42f5890319c655855d9d1e4e
wkodate <hbwandeow@gmail.com>
2020-03-07 22:15:59 +0900

    lab1

--- src/main/java/com/ververica/flinktraining/exercises/datastream_java/basics/RideCleansingExercise.java
+++ src/main/java/com/ververica/flinktraining/exercises/datastream_java/basics/RideCleansingExercise.java
@@ -16,10 +16,10 @@
 
 package com.ververica.flinktraining.exercises.datastream_java.basics;
 
-import com.ververica.flinktraining.exercises.datastream_java.sources.TaxiRideSource;
 import com.ververica.flinktraining.exercises.datastream_java.datatypes.TaxiRide;
+import com.ververica.flinktraining.exercises.datastream_java.sources.TaxiRideSource;
 import com.ververica.flinktraining.exercises.datastream_java.utils.ExerciseBase;
-import com.ververica.flinktraining.exercises.datastream_java.utils.MissingSolutionException;
+import com.ververica.flinktraining.exercises.datastream_java.utils.GeoUtils;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -30,44 +30,45 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
  * (http://training.ververica.com).
  * The task of the exercise is to filter a data stream of taxi ride records to keep only rides that
  * start and end within New York City. The resulting stream should be printed.
- *
+ * <p>
  * Parameters:
- *   -input path-to-input-file
- *
+ * -input path-to-input-file
  */
 public class RideCleansingExercise extends ExerciseBase {
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		ParameterTool params = ParameterTool.fromArgs(args);
-		final String input = params.get("input", ExerciseBase.pathToRideData);
+        ParameterTool params = ParameterTool.fromArgs(args);
+        final String input = params.get("input", ExerciseBase.pathToRideData);
 
-		final int maxEventDelay = 60;       // events are out of order by max 60 seconds
-		final int servingSpeedFactor = 600; // events of 10 minutes are served in 1 second
+        final int maxEventDelay = 60;       // events are out of order by max 60 seconds
+        final int servingSpeedFactor = 600; // events of 10 minutes are served in 1 second
 
-		// set up streaming execution environment
-		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		env.setParallelism(ExerciseBase.parallelism);
+        // set up streaming execution environment
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(ExerciseBase.parallelism);
 
-		// start the data generator
-		DataStream<TaxiRide> rides = env.addSource(rideSourceOrTest(new TaxiRideSource(input, maxEventDelay, servingSpeedFactor)));
+        // start the data generator
+        DataStream<TaxiRide> rides = env.addSource(rideSourceOrTest(
+                new TaxiRideSource(input, maxEventDelay, servingSpeedFactor)));
 
-		DataStream<TaxiRide> filteredRides = rides
-				// filter out rides that do not start or stop in NYC
-				.filter(new NYCFilter());
+        DataStream<TaxiRide> filteredRides = rides
+                // filter out rides that do not start or stop in NYC
+                .filter(new NYCFilter());
 
-		// print the filtered stream
-		printOrTest(filteredRides);
+        // print the filtered stream
+        printOrTest(filteredRides);
 
-		// run the cleansing pipeline
-		env.execute("Taxi Ride Cleansing");
-	}
+        // run the cleansing pipeline
+        env.execute("Taxi Ride Cleansing");
+    }
 
-	private static class NYCFilter implements FilterFunction<TaxiRide> {
+    private static class NYCFilter implements FilterFunction<TaxiRide> {
 
-		@Override
-		public boolean filter(TaxiRide taxiRide) throws Exception {
-			throw new MissingSolutionException();
-		}
-	}
+        @Override
+        public boolean filter(TaxiRide taxiRide) throws Exception {
+            return GeoUtils.isInNYC(taxiRide.startLon, taxiRide.startLat)
+                    && GeoUtils.isInNYC(taxiRide.endLon, taxiRide.endLat);
+        }
+    }
 
 }
--- src/main/java/com/ververica/flinktraining/exercises/datastream_java/utils/ExerciseBase.java
+++ src/main/java/com/ververica/flinktraining/exercises/datastream_java/utils/ExerciseBase.java
@@ -22,49 +22,49 @@ import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
 public class ExerciseBase {
-	public static SourceFunction<TaxiRide> rides = null;
-	public static SourceFunction<TaxiFare> fares = null;
-	public static SourceFunction<String> strings = null;
-	public static SinkFunction out = null;
-	public static int parallelism = 4;
+    public static SourceFunction<TaxiRide> rides = null;
+    public static SourceFunction<TaxiFare> fares = null;
+    public static SourceFunction<String> strings = null;
+    public static SinkFunction out = null;
+    public static int parallelism = 4;
 
-	public final static String pathToRideData = "/Users/david/stuff/flink-training/trainingData/nycTaxiRides.gz";
-	public final static String pathToFareData = "/Users/david/stuff/flink-training/trainingData/nycTaxiFares.gz";
+    public final static String pathToRideData = "/Users/wkodate/dev/flink-training-exercises/nycTaxiRides.gz";
+    public final static String pathToFareData = "/Users/wkodate/dev/flink-training-exercises/nycTaxiFares.gz";
 
-	public static SourceFunction<TaxiRide> rideSourceOrTest(SourceFunction<TaxiRide> source) {
-		if (rides == null) {
-			return source;
-		}
-		return rides;
-	}
+    public static SourceFunction<TaxiRide> rideSourceOrTest(SourceFunction<TaxiRide> source) {
+        if (rides == null) {
+            return source;
+        }
+        return rides;
+    }
 
-	public static SourceFunction<TaxiFare> fareSourceOrTest(SourceFunction<TaxiFare> source) {
-		if (fares == null) {
-			return source;
-		}
-		return fares;
-	}
+    public static SourceFunction<TaxiFare> fareSourceOrTest(SourceFunction<TaxiFare> source) {
+        if (fares == null) {
+            return source;
+        }
+        return fares;
+    }
 
-	public static SourceFunction<String> stringSourceOrTest(SourceFunction<String> source) {
-		if (strings == null) {
-			return source;
-		}
-		return strings;
-	}
+    public static SourceFunction<String> stringSourceOrTest(SourceFunction<String> source) {
+        if (strings == null) {
+            return source;
+        }
+        return strings;
+    }
 
-	public static void printOrTest(org.apache.flink.streaming.api.datastream.DataStream<?> ds) {
-		if (out == null) {
-			ds.print();
-		} else {
-			ds.addSink(out);
-		}
-	}
+    public static void printOrTest(org.apache.flink.streaming.api.datastream.DataStream<?> ds) {
+        if (out == null) {
+            ds.print();
+        } else {
+            ds.addSink(out);
+        }
+    }
 
-	public static void printOrTest(org.apache.flink.streaming.api.scala.DataStream<?> ds) {
-		if (out == null) {
-			ds.print();
-		} else {
-			ds.addSink(out);
-		}
-	}
+    public static void printOrTest(org.apache.flink.streaming.api.scala.DataStream<?> ds) {
+        if (out == null) {
+            ds.print();
+        } else {
+            ds.addSink(out);
+        }
+    }
 }
