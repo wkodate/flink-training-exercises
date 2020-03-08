15eebd32119237906edc3a4f7c70b536e1877700
wkodate <hbwandeow@gmail.com>
2020-03-08 16:19:25 +0900

    lab2

--- src/main/java/com/ververica/flinktraining/exercises/datastream_java/state/RidesAndFaresExercise.java
+++ src/main/java/com/ververica/flinktraining/exercises/datastream_java/state/RidesAndFaresExercise.java
@@ -21,7 +21,8 @@ import com.ververica.flinktraining.exercises.datastream_java.datatypes.TaxiRide;
 import com.ververica.flinktraining.exercises.datastream_java.sources.TaxiFareSource;
 import com.ververica.flinktraining.exercises.datastream_java.sources.TaxiRideSource;
 import com.ververica.flinktraining.exercises.datastream_java.utils.ExerciseBase;
-import com.ververica.flinktraining.exercises.datastream_java.utils.MissingSolutionException;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.configuration.Configuration;
@@ -33,59 +34,76 @@ import org.apache.flink.util.Collector;
 /**
  * The "Stateful Enrichment" exercise of the Flink training
  * (http://training.ververica.com).
- *
+ * <p>
  * The goal for this exercise is to enrich TaxiRides with fare information.
- *
+ * <p>
  * Parameters:
  * -rides path-to-input-file
  * -fares path-to-input-file
- *
  */
 public class RidesAndFaresExercise extends ExerciseBase {
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		ParameterTool params = ParameterTool.fromArgs(args);
-		final String ridesFile = params.get("rides", pathToRideData);
-		final String faresFile = params.get("fares", pathToFareData);
+        ParameterTool params = ParameterTool.fromArgs(args);
+        final String ridesFile = params.get("rides", pathToRideData);
+        final String faresFile = params.get("fares", pathToFareData);
 
-		final int delay = 60;					// at most 60 seconds of delay
-		final int servingSpeedFactor = 1800; 	// 30 minutes worth of events are served every second
+        final int delay = 60;                    // at most 60 seconds of delay
+        final int servingSpeedFactor = 1800;    // 30 minutes worth of events are served every second
 
-		// set up streaming execution environment
-		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		env.setParallelism(ExerciseBase.parallelism);
+        // set up streaming execution environment
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(ExerciseBase.parallelism);
 
-		DataStream<TaxiRide> rides = env
-				.addSource(rideSourceOrTest(new TaxiRideSource(ridesFile, delay, servingSpeedFactor)))
-				.filter((TaxiRide ride) -> ride.isStart)
-				.keyBy("rideId");
+        DataStream<TaxiRide> rides = env
+                .addSource(rideSourceOrTest(new TaxiRideSource(ridesFile, delay, servingSpeedFactor)))
+                .filter((TaxiRide ride) -> ride.isStart)
+                .keyBy("rideId");
 
-		DataStream<TaxiFare> fares = env
-				.addSource(fareSourceOrTest(new TaxiFareSource(faresFile, delay, servingSpeedFactor)))
-				.keyBy("rideId");
+        DataStream<TaxiFare> fares = env
+                .addSource(fareSourceOrTest(new TaxiFareSource(faresFile, delay, servingSpeedFactor)))
+                .keyBy("rideId");
 
-		DataStream<Tuple2<TaxiRide, TaxiFare>> enrichedRides = rides
-				.connect(fares)
-				.flatMap(new EnrichmentFunction());
+        DataStream<Tuple2<TaxiRide, TaxiFare>> enrichedRides = rides
+                .connect(fares)
+                .flatMap(new EnrichmentFunction());
 
-		printOrTest(enrichedRides);
+        printOrTest(enrichedRides);
 
-		env.execute("Join Rides with Fares (java RichCoFlatMap)");
-	}
+        env.execute("Join Rides with Fares (java RichCoFlatMap)");
+    }
 
-	public static class EnrichmentFunction extends RichCoFlatMapFunction<TaxiRide, TaxiFare, Tuple2<TaxiRide, TaxiFare>> {
+    public static class EnrichmentFunction extends RichCoFlatMapFunction<TaxiRide, TaxiFare, Tuple2<TaxiRide, TaxiFare>> {
 
-		@Override
-		public void open(Configuration config) throws Exception {
-			throw new MissingSolutionException();
-		}
+        private ValueState<TaxiRide> rideState;
+        private ValueState<TaxiFare> fareState;
 
-		@Override
-		public void flatMap1(TaxiRide ride, Collector<Tuple2<TaxiRide, TaxiFare>> out) throws Exception {
-		}
+        @Override
+        public void open(Configuration config) throws Exception {
+            rideState = getRuntimeContext().getState(new ValueStateDescriptor<>("ride", TaxiRide.class));
+            fareState = getRuntimeContext().getState(new ValueStateDescriptor<>("fare", TaxiFare.class));
+        }
 
-		@Override
-		public void flatMap2(TaxiFare fare, Collector<Tuple2<TaxiRide, TaxiFare>> out) throws Exception {
-		}
-	}
+        @Override
+        public void flatMap1(TaxiRide ride, Collector<Tuple2<TaxiRide, TaxiFare>> out) throws Exception {
+            TaxiFare fare = fareState.value();
+            if (fare != null) {
+                fareState.clear();
+                out.collect(new Tuple2(ride, fare));
+            } else {
+                rideState.update(ride);
+            }
+        }
+
+        @Override
+        public void flatMap2(TaxiFare fare, Collector<Tuple2<TaxiRide, TaxiFare>> out) throws Exception {
+            TaxiRide ride = rideState.value();
+            if (ride != null) {
+                rideState.clear();
+                out.collect(new Tuple2(ride, fare));
+            } else {
+                fareState.update(fare);
+            }
+        }
+    }
 }
--- src/test/java/com/ververica/flinktraining/exercises/datastream_java/state/RidesAndFaresTest.java
+++ src/test/java/com/ververica/flinktraining/exercises/datastream_java/state/RidesAndFaresTest.java
@@ -16,11 +16,11 @@
 
 package com.ververica.flinktraining.exercises.datastream_java.state;
 
+import com.google.common.collect.Lists;
 import com.ververica.flinktraining.exercises.datastream_java.datatypes.TaxiFare;
 import com.ververica.flinktraining.exercises.datastream_java.datatypes.TaxiRide;
 import com.ververica.flinktraining.exercises.datastream_java.testing.TaxiRideTestBase;
 import com.ververica.flinktraining.solutions.datastream_java.state.RidesAndFaresSolution;
-import com.google.common.collect.Lists;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.joda.time.DateTime;
 import org.junit.Test;
@@ -32,50 +32,50 @@ import static org.junit.Assert.assertEquals;
 
 public class RidesAndFaresTest extends TaxiRideTestBase<Tuple2<TaxiRide, TaxiFare>> {
 
-	static Testable javaExercise = () -> RidesAndFaresExercise.main(new String[]{});
+    static Testable javaExercise = () -> RidesAndFaresExercise.main(new String[]{});
 
 
-	final TaxiRide ride1 = testRide(1);
-	final TaxiRide ride2 = testRide(2);
-	final TaxiFare fare1 = testFare(1);
-	final TaxiFare fare2 = testFare(2);
+    final TaxiRide ride1 = testRide(1);
+    final TaxiRide ride2 = testRide(2);
+    final TaxiFare fare1 = testFare(1);
+    final TaxiFare fare2 = testFare(2);
 
-	@Test
-	public void testInOrder() throws Exception {
-		TestRideSource rides = new TestRideSource(ride1, ride2);
-		TestFareSource fares = new TestFareSource(fare1, fare2);
+    @Test
+    public void testInOrder() throws Exception {
+        TestRideSource rides = new TestRideSource(ride1, ride2);
+        TestFareSource fares = new TestFareSource(fare1, fare2);
 
-		ArrayList<Tuple2<TaxiRide, TaxiFare>> expected = Lists.newArrayList(
-				new Tuple2<>(ride1, fare1),
-				new Tuple2<>(ride2, fare2));
+        ArrayList<Tuple2<TaxiRide, TaxiFare>> expected = Lists.newArrayList(
+                new Tuple2<>(ride1, fare1),
+                new Tuple2<>(ride2, fare2));
 
-		assertEquals(expected, results(rides, fares));
-	}
+        assertEquals(expected, results(rides, fares));
+    }
 
-	@Test
-	public void testOutOfOrder() throws Exception {
-		TestRideSource rides = new TestRideSource(ride1, ride2);
-		TestFareSource fares = new TestFareSource(fare2, fare1);
+    @Test
+    public void testOutOfOrder() throws Exception {
+        TestRideSource rides = new TestRideSource(ride1, ride2);
+        TestFareSource fares = new TestFareSource(fare2, fare1);
 
-		ArrayList<Tuple2<TaxiRide, TaxiFare>> expected = Lists.newArrayList(
-				new Tuple2<>(ride1, fare1),
-				new Tuple2<>(ride2, fare2));
+        ArrayList<Tuple2<TaxiRide, TaxiFare>> expected = Lists.newArrayList(
+                new Tuple2<>(ride1, fare1),
+                new Tuple2<>(ride2, fare2));
 
-		assertEquals(expected, results(rides, fares));
-	}
+        assertEquals(expected, results(rides, fares));
+    }
 
-	private TaxiRide testRide(long rideId) {
-		return new TaxiRide(rideId, true, new DateTime(0), new DateTime(0),
-				0F, 0F, 0F, 0F, (short)1, 0, rideId);
-	}
+    private TaxiRide testRide(long rideId) {
+        return new TaxiRide(rideId, true, new DateTime(0), new DateTime(0),
+                0F, 0F, 0F, 0F, (short) 1, 0, rideId);
+    }
 
-	private TaxiFare testFare(long rideId) {
-		return new TaxiFare(rideId, 0, rideId, new DateTime(0), "", 0F, 0F, 0F);
-	}
+    private TaxiFare testFare(long rideId) {
+        return new TaxiFare(rideId, 0, rideId, new DateTime(0), "", 0F, 0F, 0F);
+    }
 
-	protected List<?> results(TestRideSource rides, TestFareSource fares) throws Exception {
-		Testable javaSolution = () -> RidesAndFaresSolution.main(new String[]{});
-		return runApp(rides, fares, new TestSink<>(), javaExercise, javaSolution);
-	}
+    protected List<?> results(TestRideSource rides, TestFareSource fares) throws Exception {
+        Testable javaSolution = () -> RidesAndFaresSolution.main(new String[]{});
+        return runApp(rides, fares, new TestSink<>(), javaExercise, javaSolution);
+    }
 
 }
